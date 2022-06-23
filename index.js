474349d584e13ecadfb2c9cc68e32f9d9bbf9257
Toniahh <buchitoniah@gmail.com>
2022-06-23 23:31:28 +0100

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var num1=parseInt(prompt( "Number 1" ));
+var num2=parseInt(prompt( "Number 2" ));
+var opr=prompt( "Operator" );
+  if(opr=="+"){
+    alert (num1 + num2);
+
+} else if(opr=="-"){
+    alert (num1 - num2);
+
+} else if(opr=="*"){
+    alert (num1 * num2);
+
+} else if(opr=="/"){
+    alert (num1 / num2);
+
+} else if(opr=="%"){
+    alert (num1 % num2);
+}
